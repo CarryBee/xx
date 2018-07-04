fe2af3f93275bcfe45dee3db3f1520d43c0276a8
Jesusalexander <brpoper@gmail.com>
2018-07-04 21:14:44 +0800

    sacve

--- wxh5/src/router.js
+++ wxh5/src/router.js
@@ -14,6 +14,7 @@ import OrderConfirm from './views/order/orderConfirm'
 import Recharge from './views/recharge'
 import myProduct from './views/myProduct'
 import Cashier from './views/Cashier'
+import PartnerIncome from './views/PartnerIncome'
 
 Vue.use(Router)
 
@@ -99,6 +100,11 @@ export default new Router({
       path: '/cashier',
       name: 'cashier',
       component: Cashier
+    },
+    {
+      path: '/income',
+      name: 'income',
+      component: PartnerIncome
     }
   ],
 })
---
+++ wxh5/src/views/PartnerIncome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="sum-wrapper">
+      <div class="income-sum">9055.00元</div>
+      <div class="ctrl">点击取现</div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'income',
+    data () {
+      return {
+        partnerSum: 9055
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped="">
+</style>
