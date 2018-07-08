663df2795d7f9dbc8b8e9de5f423168b375dc37a
karonl <karonl@qq.com>
2018-07-08 23:01:54 +0800

    整合 Partner.vue

--- wxh5/src/views/Partner.vue
+++ wxh5/src/views/Partner.vue
@@ -2,7 +2,6 @@
   <div class="home page">
 
     <div class="header-info flex-box jc-ce">
-
       <div class="user-header flex-box jc-ce ai-c">
         <div class="reward-wrapper flex-box">
           <div class="sum-award flex-box flex-direction-column flex-1 ai-c jc-ce">
@@ -10,58 +9,69 @@
             <div class="award-num">1208.03</div>
           </div>
         </div>
-        <!--
-        <div class="head">
-          <img :src="userInfo.headimgurl" alt="">
-        </div>
-        <div class="user-info t-white">
-          <div class="name f-30">{{userInfo.nickname || '未授权登录'}}</div>
-          <div class="id">超级合伙人</div>
-        </div>
-        <div class="qrcode-ctrl flex-box flex-direction-column t-white">
-          <router-link to="/myShareLink/myShareOption">
-            <i class="iconfont icon-qrcode2 f-70 ta-c"></i>
-            <div class="title">生成推广码</div>
-          </router-link>
-        </div>
-        -->
       </div>
     </div>
 
+  <div class="recommend-list">
     <div class="recommend-info flex-box flex-direction-column jc-ce">
-      <div class="recommend-sum flex-1 ta-c f-30">
-        <div class="count f-40">已推荐注册人数 <span class="f-50 t-orange">0</span></div>
-        <div class="title"></div>
+        <div class="recommend-sum flex-1 ta-c f-30">
+          <div class="recommend-title">总收益情况</div>
+        </div>
+        <div class="recommend-detail flex-1 flex-box jc-ce">
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">0</div>
+            <div class="title">今日收益</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">0</div>
+            <div class="title">近7日收益</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">0</div>
+            <div class="title">本月收益</div>
+          </div>
+        </div>
       </div>
-      <div class="recommend-detail flex-1 flex-box jc-ce">
-        <div class="detail-info ta-c">
-          <div class="count f-35 t-orange">0</div>
-          <div class="title">代理商</div>
-        </div>
-        <div class="detail-info ta-c">
-          <div class="count f-35 t-orange">0</div>
-          <div class="title">普通用户</div>
-        </div>
-        <div class="detail-info ta-c">
-          <div class="count f-35 t-orange">0</div>
-          <div class="title">下属人数</div>
-        </div>
-      </div>
-    </div>
 
-    <div class="recommend-info flex-box flex-direction-column jc-ce">
-      <div class="recommend-sum flex-1 ta-c f-30">
-        <div class="count f-40">今日收益 <span class="f-50 t-orange">0</span></div>
-        <div class="title"></div>
-      </div>
-      <div class="recommend-detail flex-1 flex-box jc-ce">
-        <div class="detail-info ta-c">
-          <div class="count f-35 t-orange">0</div>
-          <div class="title">本月收益</div>
+      <div class="recommend-info flex-box flex-direction-column jc-ce">
+        <div class="recommend-tip">最近30天</div>
+        <div class="recommend-sum flex-1 ta-c f-30">
+          <div class="recommend-title">消费分布情况</div>
         </div>
-        <div class="detail-info ta-c">
-          <div class="count f-35 t-orange">0</div>
-          <div class="title">总收益</div>
+        <div class="recommend-detail flex-1 flex-box jc-ce">
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">4</div>
+            <div class="title">普通用户数</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">2+3</div>
+            <div class="title">合伙人数</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">9</div>
+            <div class="title">总用户数</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="recommend-info flex-box flex-direction-column jc-ce">
+        <div class="recommend-tip">最近30天</div>
+        <div class="recommend-sum flex-1 ta-c f-30">
+          <div class="recommend-title">消费分成收益</div>
+        </div>
+        <div class="recommend-detail flex-1 flex-box jc-ce">
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">110</div>
+            <div class="title">普通用户</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">103</div>
+            <div class="title">合伙人</div>
+          </div>
+          <div class="detail-info ta-c flex-1">
+            <div class="count f-35 t-orange">213</div>
+            <div class="title">总用户收益</div>
+          </div>
         </div>
       </div>
     </div>
@@ -85,7 +95,7 @@
     <!--</div>-->
     <div class="content">
       <div class="entry-btn-list flex-box"></div>
-      <banner-ad-list :adList="bannerAdList"></banner-ad-list>
+      <!--<banner-ad-list :adList="bannerAdList"></banner-ad-list>-->
       <div class="banner">
         <div class="operation-title">推广收益规则</div>
         <img src="@/assets/banner1.png" />
@@ -176,8 +186,8 @@ export default {
     position: relative;
     color: #fff;
     .award-title {
-      line-height: px2rem(26);
-      font-size: px2rem(26);
+      line-height: px2rem(38);
+      font-size: px2rem(28);
     }
     .award-num {
       line-height: px2rem(80);
@@ -217,18 +227,32 @@ export default {
       border: px2rem(5) solid rgba(255, 255, 255, 0.3);
     }
   }
-  .recommend-info {
+  .recommend-list {
     position: relative;
     top: px2rem(-80);
-    padding: px2rem(20);
-    margin: px2rem(20);
-    background: #fff;
-    border-radius: px2rem(10);
-    .recommend-sum {
-      margin-bottom: px2rem(20);
-    }
-    .detail-info {
-      padding: 0 px2rem(30);
+    .recommend-info {
+      position: relative;
+      padding: px2rem(34) px2rem(60);
+      margin: px2rem(20) px2rem(20) px2rem(0) px2rem(20);
+      background: #fff;
+      border-radius: px2rem(10);
+      .recommend-tip {
+        position: absolute;
+        color: #bdbdbd;
+        font-style: italic;
+        right: px2rem(10);
+        top: px2rem(10);
+      }
+      .recommend-title {
+        font-size: px2rem(34);
+        padding-bottom: px2rem(14);
+      }
+      .recommend-sum {
+        margin-bottom: px2rem(20);
+      }
+      .detail-info {
+        padding: 0 px2rem(0);
+      }
     }
   }
 </style>
