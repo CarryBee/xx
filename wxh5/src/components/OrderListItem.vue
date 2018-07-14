4f887142c051474870b14709f9d371ab1f155c95
Jesusalexander <brpoper@gmail.com>
2018-07-14 13:15:31 +0800

    合订单列表

--- wxh5/src/components/OrderListItem.vue
+++ wxh5/src/components/OrderListItem.vue
@@ -1,63 +1,48 @@
-</<template>
-    <div class="order-item">
-        <div class="item-title flex-box jc-sb ai-c">
-          <div class="item-no">5xxxxc5218b4ff184d833d82</div>
-          <div class="item-pay">已付款</div>
-        </div>
-        <div class="item-snaps flex-box flex-direction-column">
-            <div class="snap flex-box">
-                <div class="image">image</div>
-                <div class="other flex-1">
-                  <div class="title">title</div>
-                  <div class="info">brand 10</div>
-                </div>
-                <div class="price">￥101.1234(￥0 优惠额度)</div>
-            </div>
-            <div class="snap flex-box">
-                <div class="image">image</div>
-                <div class="other flex-1">
-                  <div class="title">title</div>
-                  <div class="info">brand 10</div>
-                </div>
-                <div class="price">￥101.1234</div>
-            </div>
-        </div>
-        <div class="item-info flex-box jc-sb ai-c">
-            <div class="time">2018-7-13 00:12</div>
-            <div class="order flex-box">
-                <div class="allprice">￥1101.23</div>
-                <div class="freemach">(优惠1台)</div>
-            </div>
-        </div>
+<template>
+  <div class="order-item">
+    <div class="item-title flex-box jc-sb ai-c">
+      <div class="item-no">订单：{{item.id}}</div>
+      <div class="item-pay t-orange">已付款</div>
     </div>
+    <div class="item-snaps flex-box flex-direction-column">
+
+      <div class="snap flex-box" v-for="(snapItem, index) in item.snap">
+        <div class="left">
+          <div class="product-img-wrapper">
+            <img :src="snapItem.content[0]" alt="">
+          </div>
+        </div>
+        <div class="right flex-box flex-direction-column">
+          <div class="other flex-1">
+            <div class="title f-28 t-black">{{snapItem.title}}</div>
+            <div class="info f-26 t-9-grey">{{snapItem.text}}</div>
+          </div>
+          <div class="price f-24"><span class="snap-price t-orange">￥{{snapItem.price}}</span>(￥0 优惠额度)</div>
+        </div>
+      </div>
+
+    </div>
+    <div class="item-info flex-box jc-sb ai-c">
+      <div class="time">2018-7-13 00:12</div>
+      <div class="order flex-box">
+        <div class="allprice t-orange">￥1101.23</div>
+        <div class="freemach">(优惠1台)</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   // 订单item，包含订单里面的多个snap物品
-  data () {
-    return {
-      pay: false,
-      snap: [
-        {
-          id: '5b27dc5218b4ff184d833d82', // 商品快照的id
-          title: 'title', // 商品标题
-          text: 'text', // 商品文字描述
-          price: 101.1234, // 商品标价，如果有真实价格，则可以划线划掉。没优惠券时都显示这个价格
-          actualprice: 0, // 商品真实价格，两个价格不一样证明用了优惠券，可以标注
-          usefreemach: true, // 是否使用了优惠额度
-          rate: 10,
-          brand: 'brand', // 产家
-          productDetailImg: '/img/product.500f8864.jpg',
-          content: ['/img/product.500f8864.jpg'], // 显示图片
-          createtime: '2018-06-18T16:22:42.915Z', // 显示创建时间
-          __v: 0
-        }
-      ],
-      allprice: 100,
-      freemach: 1,
-      id: '5xxxxc5218b4ff184d833d82'
+  props: {
+    item: {
+      type: Object,
+      default: {}
     }
+  },
+  data () {
+    return {}
   }
 }
 </script>
@@ -69,16 +54,17 @@ export default {
   margin-bottom: px2rem(20);
   border-top: px2rem(2) solid #eaeaea;
   border-bottom: px2rem(2) solid #eaeaea;
-  padding: 0px px2rem(30);
   .item-title {
     height: px2rem(80);
     border-bottom: px2rem(2) solid #eaeaea;
+    padding: 0px px2rem(30);
     .item-no {}
     .item-pay {}
   }
   .item-info {
     border-top: px2rem(2) solid #eaeaea;
     min-height: px2rem(80);
+    padding: 0px px2rem(30);
     .time {}
     .order {
       .allprice {}
@@ -88,9 +74,20 @@ export default {
     }
   }
   .item-snaps {
+    padding-bottom: px2rem(30);
     .snap {
-      .image {
-        width: px2rem(100);
+      padding: 0 px2rem(30);
+      margin-top: px2rem(20);
+      background: #f9f9f9;
+      .product-img-wrapper {
+        height: px2rem(150);
+        width: px2rem(150);
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: px2rem(10);
+          background: #f9f9f9;
+        }
       }
       .other {
         padding-left: px2rem(20);
@@ -98,6 +95,10 @@ export default {
         .info {}
       }
       .price {}
+      .snap-price {
+        padding-left: px2rem(20);
+        padding-right: px2rem(10);
+      }
     }
   }
 }
