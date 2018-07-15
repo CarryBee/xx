f303ff83f16614f122009dc70ceeb98ef959e187
karonl <karonl@qq.com>
2018-07-15 15:24:09 +0800

    引入 iview 构建中控

---
+++ be/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <i-input>
+            <span slot="prepend">http://</span>
+            <span slot="append">.cn</span>
+        </i-input>
+    </div>    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
