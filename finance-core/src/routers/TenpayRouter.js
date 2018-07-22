4fc99ace229d87d93776d650f0158371d369844e
karonl <karonl@qq.com>
2018-07-23 00:41:54 +0800

    完成三种模式的下单路由

--- finance-core/src/routers/TenpayRouter.js
+++ finance-core/src/routers/TenpayRouter.js
@@ -1,10 +1,12 @@
 const tenpay = require("tenpay");
-// const config = require("../config.js");
+const config = require("../config.js");
+const ERO = require("../tools/Errorbody");
 const Router = require("koa-router");
 const Invoice = require("../tools/financebox/Invoice") // 金额校验工具
+const OrderModule = require("../modules/OrderModule");
 const $ = new Router();
 
-/*
+
 const tenpayConfig = {
   appid: config.appid,
   mchid: config.mchid,
@@ -14,98 +16,134 @@ const tenpayConfig = {
   spbill_create_ip: config.spbill_create_ip
 }
 const tenpayApi = new tenpay(tenpayConfig);
-*/
 
-//================  微信统一预付单接口
+
+//================  微信统一预付单接口  ================
+async function tenpayParams(out_trade_no, event, amount, user, params) {
+  const openid = user.openid;
+  const userid = user.userid;
+  const level = user.level;
+
+  // 一大堆校验机制
+  if (!amount || amount < 0) { throw new Error('无效支付金额'); }
+  if (!userid || !level) { throw new Error('无效用户'); }
+  if (!openid) { throw new Error('非微信用户无法使用微信充值'); }
+  if (["rechange", "payorder", "payvip"].indexOf(event) === -1) {
+    throw new Error('非允许支付业务类型');
+  }
+  const invo = new Invoice();
+  invo.plusnum = amount;
+  amount = invo.amount;
+  // 一大堆校验机制
+  const attach = { // 自定义参数，支付微信回调时候要用
+    event,
+    openid,
+    userid,
+    level,
+    amount,
+    ...params
+  };
+
+  const payConfig = {
+    out_trade_no: out_trade_no,
+    body: '四汇金融充值购物服务:' + event,
+    total_fee: amount,
+    attach
+  }
+  return await tenpayApi.unifiedOrder(payConfig);
+}
 /**
  * 充值接口
  * curl http://localhost:3000/tenpay/payRecharge -d nickname="xxxx" -H 'token: xxxx'
  */
 $.post('/payrecharge', async ctx => {
-  ctx.body = ctx.header;
-});
+  try {
+    const amount = ctx.prb.postParam("amount"); // 充值的钱，用户输入的
+    const user = ctx.prb.getCurrentUser(); // 得到用户信息
+    const out_trade_no = "userid_" + user.userid; // 记录在微信里面的下单号
 
+    const res = await tenpayParams(out_trade_no, "recharge", amount, user);
+    ctx.body = ERO(0, "统一下单", res);
+  } catch(err) {
+    ctx.body = ERO(501, "统一下单", "生成预付错误", err.message);
+  }
+});
 /**
  * 支付订单接口
  */
-$.post('/payRecharge', async ctx => {
+$.post('/payorder', async ctx => {
+  try {
+    const orderid = ctx.prb.postParam("orderid"); // 订单id，根据选择购物生成
+    const user = ctx.prb.getCurrentUser(); // 得到用户信息
+    
+    const order = await OrderModule.getUnpaidOrderByID(orderid); // 已支付订单会直接失败
+    const out_trade_no = "orderid_" + order._id; // 记录在微信里面的下单号
+    const amount = order.allprice; // 根据订单id计算价格，
 
+    const res = await tenpayParams(out_trade_no, "payorder", amount, user, { orderid } );
+    ctx.body = ERO(0, "统一下单", res);
+  } catch(err) {
+    ctx.body = ERO(501, "统一下单", "生成预付错误", err.message);
+  }
 });
-
 /**
- * 预付会员接口
+ * 支付会员接口
  */
-$.post('/payRecharge', async ctx => {
+$.post('/payvip', async ctx => {
+  try {
+    const aimlevel = ctx.prb.postParam("aimlevel"); // 目标级别，根据菜单中选择生成
+    const user = ctx.prb.getCurrentUser(); // 得到用户信息
+  
+    const amount = 100; // 根据等级计算差价, [待修改]
+    const out_trade_no = "userid_" + user.userid; // 记录在微信里面的下单号
 
+    const res = await tenpayParams(out_trade_no, "payvip", amount, user, { aimlevel } );
+    ctx.body = ERO(0, "统一下单", res);
+  } catch(err) {
+    ctx.body = ERO(501, "统一下单", "生成预付错误", err.message);
+  }
 });
 
-async function tenpayParams (event, ctx) {
-  const user = ctx.prb.getCurrentUser(); // 得到用户信息
-  let amount = ctx.prb.post().amount; // 充值的钱
-  const openid = user.openid;
-  const userid = user.userid;
-  const level = user.level;
-
-  if (!amount || amount < 0) { throw new Error('无效支付金额'); }
-  if (!userid || !level) { throw new Error('无效用户'); }
-  if (!openid) { throw new Error('非微信用户无法使用微信充值'); }
-  if(["rechange", "payorder", "payvip"].indexOf(event) === -1) { 
-    throw new Error('非允许支付业务类型'); 
-  }
-
-  const invo = new Invoice();
-  invo.plusnum = amount;
-  amount = invo.amount;
-  
-  let params = {
-    out_trade_no: 'test_unified_001',
-    body: '商城充值',
-    total_fee: amount,
-    amount,
-    event: 'rechange',
-    ...openid
-  }
-  let unifiedOrderRes = await tenpayApi.unifiedOrder(params)
-  // const sandboxAPI = await tenpay.sandbox(tenpayConfig);
-  // let unifiedOrderRes = await sandboxAPI.unifiedOrder(params)
-  console.log('unifiedOrderRes', unifiedOrderRes)
-  ctx.body = HR({
-    data: {
-      unifiedOrderRes
-    }
-  })
-} 
+//================  微信统一预付单接口  ================
 
 
+
+
+
+
+
+//================  微信支付回调函数入口  ================
 
 // 微信支付通知，回调函数
 /**
  *
  * 微信支付回调
- * 三个入口
+ * 三种模式入口
  *
  */
 $.get('/weixintenpaycallback', async ctx => {// (充值正常模式) 额外驱动
 
-    const body = {
-        rechange: "ok",
-        payorder: "ok",
-        payvip: "ok"
-    }
-    console.log('rechange', ctx)
-    // 拿到回调后增加对应钱包的钱，（+）{userid:"karonl", event:"rechange", params:undefined, amount:100}
-    ctx.body.rechange = "ok";
-    // 如果有订单编号，则执行 payorder 的扣款逻辑 {userid:"karonl", event:"payorder", params:{orderid:"222sfasdf"}, amount:100}
-    let event = "";
-    if(event == "payorder") {
-        payorder();
-        ctx.body.payorder = "ok";
-    }
-    // 如果有升级编号，则执行 payvip 的扣款逻辑 {userid:"karonl", event:"payvip", params:{aimlevel:12}, amount:100}
-    if(event == "payvip") {
-        payvip();
-        ctx.body.payvip = "ok";
-    }
+  const body = {
+    rechange: "ok",
+    payorder: "ok",
+    payvip: "ok"
+  }
+  console.log('rechange', ctx)
+  // 拿到回调后增加对应钱包的钱，（+）{userid:"karonl", event:"rechange", amount:100}
+  ctx.body.rechange = "ok";
+  // 如果有订单编号，则执行 payorder 的扣款逻辑 {userid:"karonl", event:"payorder", orderid:"222sfasdf" }
+  let event = "";
+  if (event == "payorder") {
+    payorder();
+    ctx.body.payorder = "ok";
+  }
+  // 如果有升级编号，则执行 payvip 的扣款逻辑 {userid:"karonl", event:"payvip", aimlevel:12 }
+  if (event == "payvip") {
+    payvip();
+    ctx.body.payvip = "ok";
+  }
 });
+
+//================  微信支付回调函数入口  ================
 
 module.exports = $.routes();
--- finance-core/src/tools/ParamsBox.js
+++ finance-core/src/tools/ParamsBox.js
@@ -1,39 +1,47 @@
 const jv = require("./jwtcontrol");
 class ParamsBox {
 
-    // 过滤器
+  // 过滤器
 
-    constructor(ctx) {
-        this.ctx = ctx;
-        return this;
-    }
+  constructor(ctx) {
+    this.ctx = ctx;
+    return this;
+  }
 
-    getCurrentUser() {
-        const user = new jv(this.ctx).verify(); // 针对 header token
-        return user; // 返回加密内容
-    }
+  getCurrentUser() {
+    const user = new jv(this.ctx).verify(); // 针对 header token
+    return user; // 返回加密内容
+  }
 
-    get() {
-        let query = this.ctx.query;
-        if(!query) throw new Error("ctx.get params is null");
-        // { ss: 'ss' }
-        return query;
-    }
-    
-    post() {
-        if(!this.ctx.request) throw new Error("ctx.post params is null");
-        const body = this.ctx.request.body;
-        if(!body) throw new Error("ctx.post params is null");
-        // { ss: 'ss' }
-        return body;
-    }
+  get() {
+    let query = this.ctx.query;
+    if (!query) throw new Error("ctx.get params is null");
+    // { ss: 'ss' }
+    return query;
+  }
 
-    static routes() {
-        return async (ctx, next) => {
-            const prb = new ParamsBox(ctx);
-            ctx.prb = prb;
-            await next();
-        }
+  post() {
+    if (!this.ctx.request) throw new Error("ctx.post params is null");
+    const body = this.ctx.request.body;
+    if (!body) throw new Error("ctx.post params is null");
+    // { ss: 'ss' }
+    return body;
+  }
+
+  postParam(name) {
+    if (!this.ctx.request) throw new Error("ctx.post params is null");
+    const body = this.ctx.request.body;
+    if (!body) throw new Error("ctx.post params is null");
+    if (body[name] === null || body[name] === undefined) throw new Error("ctx.post params " + name + " is null");
+    return body[name];
+  }
+
+  static routes() {
+    return async (ctx, next) => {
+      const prb = new ParamsBox(ctx);
+      ctx.prb = prb;
+      await next();
     }
+  }
 }
 module.exports = ParamsBox;
