721fc041cf255faf60f74e46ca7d51193e7358b2
karonl <karonl@qq.com>
2018-07-25 23:50:37 +0800

    前端界面

--- be/src/App.vue
+++ be/src/App.vue
@@ -1,72 +1,49 @@
 <template>
-  <div id="app">
-    <Header :style="{padding: 0}" class="layout-header-bar">
-      <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '15px 15px'}" type="navicon-round" size="24"></Icon>
-      <div class="logo-big">
-        <img src="@/assets/logo.png" />
-      </div>
-    </Header>
-    <Layout>
-      <Sider :style="{background: '#fff'}" ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-        <Menu :style="{marginRight: '-1px'}" active-name="1-2" :open-names="['1','2']" theme="light" width="auto" :class="menuitemClasses">
-          <Submenu name="1">
-            <template slot="title">
-              <Icon type="ios-paper"></Icon><span>文章管理</span>
-            </template>
-            <menu-item name="1-1"><span>文章管理</span></menu-item>
-            <menu-item name="1-2"><span>文章管理</span></menu-item>
-            <menu-item name="1-3"><span>文章管理</span></menu-item>
-          </Submenu>
-          <Submenu name="2">
-            <template slot="title">
-            <Icon type="ios-people"></Icon><span>文章管理</span>
-            </template>
-            <menu-item name="2-1"><span>文章管理</span></menu-item>
-            <menu-item name="2-2"><span>文章管理</span></menu-item>
-          </Submenu>
-          <Submenu name="3">
-            <template slot="title">
-              <Icon type="stats-bars"></Icon><span>文章管理</span>
-            </template>
-            <menu-group title="使用">
-              <menu-item name="3-1"><span>文章管理</span></menu-item>
-              <menu-item name="3-2"><span>文章管理</span></menu-item>
-              <menu-item name="3-3"><span>文章管理</span></menu-item>
-            </menu-group>
-            <menu-group title="留存">
-              <menu-item name="3-4"><span>文章管理</span></menu-item>
-              <menu-item name="3-5"><span>文章管理</span></menu-item>
-            </menu-group>
-          </Submenu>
-        </Menu>
-      </Sider>
-      <Layout>
-        <Content :style="{margin: '20px', minHeight: '800px'}">
-          Content
-        </Content>
-      </Layout>
-    </Layout>
-  </div>
+    <div id="app">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1" :style="{display: 'flex'}">
+                    <div class="logo-big">
+                      <img src="@/assets/logo.png" />
+                    </div>
+                    <div class="layout-nav">
+                        <menu-item name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            Item 1
+                        </menu-item>
+                        <menu-item name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            Item 2
+                        </menu-item>
+                        <menu-item name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            Item 3
+                        </menu-item>
+                        <menu-item name="4">
+                            <Icon type="ios-paper"></Icon>
+                            Item 4
+                        </menu-item>
+                    </div>
+                </Menu>
+            </Header>
+            <Layout>
+                <Sider hide-trigger :style="{background: '#fff'}">
+                    <Menulist></Menulist>
+                </Sider>
+                <Layout :style="{padding: '0 24px 24px'}">
+                    <Content :style="{padding: '24px', minHeight: '800px', background: '#fff'}">
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    </div>
 </template>
 <script>
+import Menulist from "./components/Menulist.vue";
 export default {
-  data() {
-    return {
-      isCollapsed: false
-    };
-  },
-  computed: {
-    rotateIcon() {
-      return ["menu-icon", this.isCollapsed ? "rotate-icon" : ""];
-    },
-    menuitemClasses() {
-      return ["menu-item", this.isCollapsed ? "collapsed-menu" : ""];
-    }
-  },
-  methods: {
-    collapsedSider() {
-      this.$refs.side1.toggleCollapse();
-    }
+  components: {
+    Menulist
   }
 };
 </script>
@@ -79,63 +56,35 @@ export default {
   background: #f5f7f9;
   overflow: hidden;
 }
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
+.layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
 }
-.layout-header-bar {
-  display: flex;
-  background: #49505F;
-  color: #fff;
-  height: 55px;
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+.layout-logo {
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
 }
 .logo-big {
-  padding: 4px 10px;
+  flex: 200px;
+  width: 200px;
   height: auto;
+  padding: 12px 0px;
 }
 .logo-big img {
   height: 30px;
 }
-.layout-logo-left {
-  width: 90%;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  margin: 15px auto;
-}
-.menu-icon {
-  transition: all 0.3s;
-}
-.rotate-icon {
-  transform: rotate(-90deg);
-}
-.menu-item span {
-  display: inline-block;
-  overflow: hidden;
-  width: 69px;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  vertical-align: bottom;
-  transition: width 0.2s ease 0.2s;
-}
-.menu-item i {
-  transform: translateX(0px);
-  transition: font-size 0.2s ease, transform 0.2s ease;
-  vertical-align: middle;
-  font-size: 16px;
-}
-.collapsed-menu span {
-  width: 0px;
-  transition: width 0.2s ease;
-}
-.collapsed-menu i {
-  transform: translateX(9px);
-  transition: font-size 0.2s ease 0.2s, transform 0.2s ease 0.2s;
-  vertical-align: middle;
-  font-size: 22px;
+.layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
 }
 </style>
--- be/src/components/Menulist.vue
+++ be/src/components/Menulist.vue
@@ -1,48 +1,52 @@
 <template>
-  <Menu theme="light" :open-names="['1']" active-name="1-2">
+  <Menu active-name="1-2" :open-names="['1','2','3','4','5']" theme="light" width="auto">
     <Submenu name="1">
       <template slot="title">
-        <icon type="ios-paper"></icon>资金系统
+        <Icon type="ios-paper"></Icon><span>用户管理</span>
       </template>
-      <menu-item name="1-1">用户管理</menu-item>
-      <menu-item name="1-2">流水管理</menu-item>
-      <menu-item name="1-3">分成管理</menu-item>
+      <menu-item name="1-1"><span>用户列表</span></menu-item>
+      <menu-item name="1-2"><span>用户资金</span></menu-item>
+      <menu-item name="1-3"><span>机器情况</span></menu-item>
     </Submenu>
     <Submenu name="2">
       <template slot="title">
-        <Icon type="ios-people"></Icon>用户管理
+        <Icon type="ios-people"></Icon><span>授权代理</span>
       </template>
-      <menu-item name="2-1">新增用户</menu-item>
-      <menu-item name="2-2">活跃用户</menu-item>
+      <menu-item name="2-1"><span>授权用户</span></menu-item>
+      <menu-item name="2-2"><span>代理列表</span></menu-item>
     </Submenu>
     <Submenu name="3">
       <template slot="title">
-        <Icon type="stats-bars"></Icon>统计分析
+        <Icon type="stats-bars"></Icon><span>商城业务</span>
       </template>
-      <menu-group title="使用">
-        <menu-item name="3-1">新增和启动</menu-item>
-        <menu-item name="3-2">活跃分析</menu-item>
-        <menu-item name="3-3">时段分析</menu-item>
+      <menu-group title="订单">
+        <menu-item name="3-1"><span>订单统计</span></menu-item>
+        <menu-item name="3-2"><span>订单查询</span></menu-item>
       </menu-group>
-      <menu-group title="留存">
-        <menu-item name="3-4">用户留存</menu-item>
-        <menu-item name="3-5">流失用户</menu-item>
+      <menu-group title="商城">
+        <menu-item name="3-4"><span>物品上架</span></menu-item>
+        <menu-item name="3-5"><span>物品管理</span></menu-item>
       </menu-group>
+    </Submenu>
+    <Submenu name="4">
+      <template slot="title">
+        <Icon type="ios-people"></Icon><span>分成设置</span>
+      </template>
+      <menu-item name="4-1"><span>刷卡管理</span></menu-item>
+      <menu-item name="4-2"><span>提现管理</span></menu-item>
+    </Submenu>
+    <Submenu name="5">
+      <template slot="title">
+        <Icon type="ios-people"></Icon><span>报表系统</span>
+      </template>
+      <menu-item name="4-1"><span>刷卡流水</span></menu-item>
+      <menu-item name="4-2"><span>提现流水</span></menu-item>
     </Submenu>
   </Menu>
 </template>
 
 <script>
-import { Menu, Submenu, Icon, MenuItem, MenuGroup } from 'iview';
 export default {
-  name: "menulist",
-  components: {
-    Menu,
-    Submenu,
-    Icon,
-    MenuItem,
-    MenuGroup
-  }
 };
 </script>
 
